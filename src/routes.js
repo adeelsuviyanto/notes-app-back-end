6b1cad75b751ae8f1f07ce422ffbdb653734f52d
adeelsuviyanto <adeelsuviyanto@gmail.com>
2022-03-15 13:19:30 +0700

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,45 @@
+const { addNoteHandler, getAllNotesHandler, getNoteByIdHandler, editNoteByIdHandler } = require('./handler')
+const routes = [
+  {
+    method: 'POST',
+    path: '/notes',
+    handler: addNoteHandler,
+    options: {
+      cors: {
+        origin: ['*']
+      }
+    }
+  },
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: getAllNotesHandler,
+    options: {
+      cors: {
+        origin: ['*']
+      }
+    }
+  },
+  {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: getNoteByIdHandler,
+    options: {
+      cors: {
+        origin: ['*']
+      }
+    }
+  },
+  {
+    method: 'PUT',
+    path: '/notes/{id}',
+    handler: editNoteByIdHandler,
+    options: {
+      cors: {
+        origin: ['*']
+      }
+    }
+  }
+]
+
+module.exports = routes
